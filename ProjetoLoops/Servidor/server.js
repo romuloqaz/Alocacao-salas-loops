397e25a1fc7ca00fd0024c7828e217cb8823ddf3
Klihsman Freitas <klihsman2.0@gmail.com>
2019-11-03 18:47:17 -0300

    Trabalhando no controle da tela de login

--- ProjetoLoops/Servidor/server.js
+++ ProjetoLoops/Servidor/server.js
@@ -3,10 +3,11 @@ const porta = 3003
 const express = require('express');
 const app = express();
 const path = require('path');
+const body = require('body-parser');
 
 app.use(express.static('/home/klihsman/Alocacao-salas-loops/ProjetoLoops/Telas/TelaLogin'))
 app.use(express.static('/home/klihsman/Alocacao-salas-loops/ProjetoLoops/Telas/TelaCadastro'))
-
+app.use(express.static('/home/klihsman/Alocacao-salas-loops/ProjetoLoops/Servidor/index.js'))
 app.get('/',(req, res, next)=>{
     res.sendFile(path.join('/home/klihsman/Alocacao-salas-loops/ProjetoLoops/Telas/TelaLogin/telaLogin.html'))
 })
--- ProjetoLoops/Telas/TelaLogin/telaLogin.js
+++ ProjetoLoops/Telas/TelaLogin/telaLogin.js
@@ -1,5 +1,15 @@
 let butt = document.getElementById('cadastro');
+let buttLog = document.getElementById('botao')
+const banco = require('../../Servidor/index')
+banco.cliente.connect();
+
+
+
 
 butt.addEventListener('click', ()=>{
-    window.location.href = "localhost:3003/cadastro"
+    window.location.href="http://localhost:3003/cadastro"
+})
+
+buttLog.addEventListener('click', ()=>{
+    banco.cliente.query("")
 })
