0e7a14fefa229d731ef9debf48ea53af1a9a815a
Antoniel Damiao <nielpoow@gmail.com>
2019-11-09 09:55:41 -0300

    adicionado os scripts de criação do banco

---
+++ ProjetoLoops/ScriptsSQL/Scripts de criação.sql
@@ -0,0 +1,29 @@
+create table servidor(
+    matricula int(20),
+    nome varchar(100) not null,
+    senha varchar(20) not null,
+    cargo varchar(40) not null,
+	email varchar(100) not null,
+    primary key(matricula)
+);
+
+create table local (
+	id serial,
+	nome varchar(50),
+	tipo varchar(50),
+    descricao varchar(200),
+	primary key(id)
+	);
+
+create table reserva(
+    id serial,
+    servidor int(20),	
+	local serial,
+	dataReserva date,
+    horarioInicio integer,
+    horariofim integer,
+    turno varchar(10),
+	foreign key (servidor)references servidor(matricula),
+    foreign key (local) references local(id)
+	) 
+
